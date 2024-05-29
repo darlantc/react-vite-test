7c141fc8558b313df2465f2f22b341a0f3ed18cf
Darlan Tódero ten Caten <darlantc@me.com>
2024-05-29 11:44:56 -0300

    added log

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+console.log("load react");
+
 ReactDOM.createRoot(document.getElementById("nav")!).render(
     <React.StrictMode>
         <App />
